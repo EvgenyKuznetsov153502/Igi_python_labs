160bc071607a1035332464cb39ef40c8796206ce
eugene <15350071@study.bsuir.by>
2023-02-28 00:05:02 +0300

    added task2

--- Lab_1/main.py
+++ Lab_1/main.py
@@ -1,3 +1,25 @@
-import task1
+import say_hello
+import calculator
 
-task1.print_hello()
+say_hello.print_hello()
+
+num1 = None
+num2 = None
+
+while True:
+    a = input("enter the first number: ")
+
+    if a.isdigit():
+        num1 = int(a)
+        break
+
+while True:
+    b = input("enter the second number: ")
+
+    if b.isdigit():
+        num2 = int(b)
+        break
+
+operation = input("choose an operation (add, sub, mult or div): ")
+result = calculator.calculate(num1, num2, operation)
+print("result:", result)
