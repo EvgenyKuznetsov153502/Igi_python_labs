1ae1e828b808196cfbd38811d2f54d9c02eaf590
eugene <15350071@study.bsuir.by>
2023-03-31 16:53:49 +0300

    add check input

--- Lab_2/Task2/CLI_program.py
+++ Lab_2/Task2/CLI_program.py
@@ -4,7 +4,6 @@ class Storage:
     def __init__(self):
         self.cur_user: str = ""
         self.cur_container: set = set()
-        self.storage: dict[str, set] = dict[str, set]()
 
     """prints all commands"""
     @staticmethod
@@ -18,45 +17,92 @@ class Storage:
               ' print each found or “No such elements” if nothing is;')
         print('save/load – save container to file/load container from file')
         print('switch – switches to another user')
+        print('stop- exit the program')
+
+    @staticmethod
+    def check_input_non_arg(len_list_of_words: int, name_of_com: str):
+        if len_list_of_words != 1:
+            print(f'Command \'{name_of_com}\' does not support arguments')
+            return False
+        else:
+            return True
+
+    @staticmethod
+    def check_input_one_arg(len_list_of_words: int, name_of_com: str):
+        if len_list_of_words > 2:
+            print(f'Error. Command \'{name_of_com}\' сan takes only 1 argument. You entered {len_list_of_words - 1} '
+                  f'arguments')
+            return False
+        if len_list_of_words == 1:
+            print(f'Error. Argument not found. Command \'{name_of_com}\' should takes 1 argument')
+            return False
+        return True
+
+    @staticmethod
+    def check_input_many_arg(len_list_of_words: int, name_of_com: str):
+        if len_list_of_words == 1:
+            print(f'Error. Arguments not found. Command \'{name_of_com}\' takes 1 or more arguments')
+            return False
+        else:
+            return True
 
     def run(self):
-
+        print("Welcome to storage for unique items")
+        print("To see the list of commands type help")
+        print('To exit the program enter stop')
 
         while True:
+
             message = input('> ')
             list_of_words = message.split()
-            if len(list_of_words) == 0:
+            len_list_of_words = len(list_of_words)
+
+            if len_list_of_words == 0:
                 print('Error empty input. Try again.')
                 continue
 
             match list_of_words[0]:
                 case 'add':
-                    print('add')
+                    if self.check_input_many_arg(len_list_of_words, 'add'):
+                        print('add')
+
                 case 'remove':
-                    print('remove')
+                    if self.check_input_one_arg(len_list_of_words, 'remove'):
+                        print('remove')
+
                 case 'find':
-                    print('find')
+                    if self.check_input_many_arg(len_list_of_words, 'find'):
+                        print('find')
+
                 case 'list':
-                    print('list')
+                    if self.check_input_non_arg(len_list_of_words, 'list'):
+                        print('list')
+
                 case 'grep':
-                    print('grep')
+                    if self.check_input_one_arg(len_list_of_words, 'grep'):
+                        print('grep')
+
                 case 'save':
-                    print('save')
+                    if self.check_input_non_arg(len_list_of_words, 'save'):
+                        print('save')
+
                 case 'load':
-                    print('load')
+                    if self.check_input_one_arg(len_list_of_words, 'load'):
+                        print('load')
+
                 case 'switch':
-                    print('switch')
+                    if self.check_input_one_arg(len_list_of_words, 'switch'):
+                        print('switch')
+
                 case 'help':
-                    if len(list_of_words) != 1:
-                        print('command \'help\' does not support arguments')
-                        continue
-                    self.print_help()
+                    if self.check_input_non_arg(len_list_of_words, 'help'):
+                        self.print_help()
+
                 case 'stop':
-                    if len(list_of_words) != 1:
-                        print('command \'stop\' does not support arguments')
-                        continue
-                    print('stop program')
-                    break
+                    if self.check_input_non_arg(len_list_of_words, 'stop'):
+                        print('Stop program')
+                        break
+
                 case _:
                     print('Error input. Try again')
 
