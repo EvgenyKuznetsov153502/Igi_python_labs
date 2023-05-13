7dbfdc4376174aa1588d638625963b1d27ab98e4
eugene <15350071@study.bsuir.by>
2023-05-13 17:03:17 +0300

    add method from_dict

--- Lab_3/Serializer/dict_serializer.py
+++ Lab_3/Serializer/dict_serializer.py
@@ -114,7 +114,6 @@ class DictSerializer:
             return {cls.TYPE_KW: cls.OBJECT_KW,
                     cls.SOURCE_KW: source}
 
-
     @classmethod
     def __get_gvars(cls, func, is_inner_func):
         name = func.__name__
@@ -159,6 +158,102 @@ class DictSerializer:
                     dct2[cls.to_dict(key)] = cls.to_dict(value)
 
         return dct2
+
+    @classmethod
+    def from_dict(cls, obj, is_dict=False):
+
+        if is_dict:
+            return {cls.from_dict(item[0]): cls.from_dict(item[1]) for item in obj}
+
+        if type(obj) not in (dict, list):
+            return obj
+
+        elif type(obj) is list:
+            return [cls.from_dict(o) for o in obj]
+
+        else:
+            obj_type = obj[cls.TYPE_KW]
+            obj_source = obj[cls.SOURCE_KW]
+
+            if obj_type == dict.__name__:
+                return cls.from_dict(obj_source, is_dict=True)
+
+            # Key - type name, value - type itself. Calling by type name returns that type.
+            # This is necessary for the same creation of simple collections.
+            cols_dict = {t.__name__: t for t in [set, frozenset, tuple, bytes, bytearray]}
+            if obj_type in cols_dict:
+                return cols_dict[obj_type](cls.from_dict(obj_source))
+
+            if obj_type == complex.__name__:
+                return obj_source[complex.real.__name__] + \
+                    obj_source[complex.imag.__name__] * 1j
+
+            if obj_type == moduletype.__name__:
+                return __import__(obj_source)
+
+            if obj_type == codetype.__name__:
+                return codetype(*[cls.from_dict(obj_source[prop]) for prop in CODE_PROPS])
+
+            if obj_type == celltype.__name__:
+                return celltype(cls.from_dict(obj_source))
+
+            if obj_type == smethodtype.__name__:
+                return staticmethod(cls.from_dict(obj_source))
+
+            if obj_type == cmethodtype.__name__:
+                return classmethod(cls.from_dict(obj_source))
+
+            if obj_type == functype.__name__:
+                code = cls.from_dict(obj_source[cls.CODE_KW])
+                gvars = cls.from_dict(obj_source[cls.GLOBALS_KW])
+                name = cls.from_dict(obj_source[cls.NAME_KW])
+                defaults = cls.from_dict(obj_source[cls.DEFAULTS_KW])
+                closure = cls.from_dict(obj_source[cls.CLOSURE_KW])
+
+                # If there are suitable global variables, they are replaced.
+                for key in gvars:
+                    if key in code.co_name and key in globals():
+                        gvars[key] = globals()[key]
+
+                func = functype(code, gvars, name, defaults, closure)
+
+                # Restoring recursion
+                if func.__name__ in gvars:
+                    func.__globals__.update({func.__name__: func})
+
+                return func
+
+            if obj_type == type.__name__:
+                name = cls.from_dict(obj_source[cls.NAME_KW])
+                bases = cls.from_dict(obj_source[cls.BASES_KW])
+                dct = obj_source[cls.DICT_KW]
+                dct = {cls.from_dict(item[0]): cls.from_dict(item[1]) for item in dct.items()}
+
+                cl = type(name, bases, dct)
+
+                # Restore a reference to the current class in the nested method __globals__
+                for attr in cl.__dict__.values():
+                    if inspect.isroutine(attr):
+                        if type(attr) in (smethodtype, classmethod):
+                            fglobs = attr.__func__.__globals__
+                        else:
+                            fglobs = attr.__globals__
+
+                        for gv in fglobs.keys():
+                            if gv == cl.__name__:
+                                fglobs[gv] = cl
+
+                return cl
+
+            else:
+                clas = cls.from_dict(obj_source[cls.CLASS_KW])
+                dct = obj_source[cls.DICT_KW]
+                dct = {cls.from_dict(item[0]): cls.from_dict(item[1]) for item in dct.items()}
+
+                o = object.__new__(clas)
+                o.__dict__ = dct
+
+                return o
 
 
 
