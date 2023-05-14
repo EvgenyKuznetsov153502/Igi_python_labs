e9901a46ef95837477add06485bac87fba65c339
eugene <15350071@study.bsuir.by>
2023-05-14 13:42:39 +0300

    update JsonSerializer

--- Lab_3/Serializer/json_serializer.py
+++ Lab_3/Serializer/json_serializer.py
@@ -31,6 +31,10 @@ class JsonSerializer(BaseSerializer):
     VALUE_PATTERN = fr"\s*({ELEMENTARY_TYPES_PATTERN}|" + \
                     fr"{ARRAY_PATTERN}|{OBJECT_PATTERN})\s*"
 
+    def dumps(self, obj) -> str:
+        obj = DictSerializer.to_dict(obj)
+        return self.__dumps_from_dict(obj)
+
     def __dumps_from_dict(self, obj) -> str:
         if type(obj) in (int, float):
             return str(obj)
@@ -52,5 +56,68 @@ class JsonSerializer(BaseSerializer):
                                     f"{self.__dumps_from_dict(item[1])}" for item in obj.items()]) + '}'
         else:
             raise ValueError
+
+    def loads(self, string: str):
+        obj = self.__loads_to_dict(string)
+        return DictSerializer.from_dict(obj)
+
+    def __loads_to_dict(self, string: str):
+        string = string.strip()
+
+        # Int
+        match = re.fullmatch(self.INT_PATTERN, string)
+        if match:
+            return int(match.group(0))
+
+        # Float
+        match = re.fullmatch(self.FLOAT_PATTERN, string)
+        if match:
+            return float(match.group(0))
+
+        # Bool
+        match = re.fullmatch(self.BOOL_PATTERN, string)
+        if match:
+            return match.group(0) == self.TRUE_LITERAL
+
+        # Str
+        match = re.fullmatch(self.STRING_PATTERN, string)
+        if match:
+            ans = match.group(0)
+            ans = self.__unmask_quotes(ans)
+            return ans[1:-1]
+
+        # None
+        match = re.fullmatch(self.NULL_PATTERN, string)
+        if match:
+            return None
+
+            # List
+        if string[0] == '[' and string[-1] == ']':
+            string = string[1:-1]
+            matches = regex.findall(self.VALUE_PATTERN, string)
+            return [self.__loads_to_dict(match[0]) for match in matches]
+
+        # Dict
+        if string[0] == '{' and string[-1] == '}':
+            string = string[1:-1]
+            matches = regex.findall(self.VALUE_PATTERN, string)
+
+            # Variable matches will store key-value pairs in one row. Elements with
+            # even indexes are keys, those with odd indexes are values.
+            return {self.__loads_to_dict(matches[i][0]):
+                    self.__loads_to_dict(matches[i + 1][0]) for i in range(0, len(matches), 2)}
+
+        else:
+            raise ValueError
+
+    @staticmethod
+    def __mask_quotes(string: str) -> str:
+        return string.replace('\\', "\\\\").replace('"', r"\"").replace("'", r"\'")
+
+    @staticmethod
+    def __unmask_quotes(string: str) -> str:
+        return string.replace('\\\\', "\\").replace(r"\"", '"').replace(r"\'", "'")
+
+
 
 
